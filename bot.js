962c926e9e643f33ca1ae823ce85df95732398bc
Richie Goulazian <rgoulazian@gmail.com>
2019-02-25 20:55:55 -0500

    Reworking command system

--- bot.js
+++ bot.js
@@ -6,20 +6,56 @@ const client = new Discord.Client();
 client.on('ready', function() { console.log("Bot connected.") });
 client.login(auth.token);
 
-//Load commands
-var commands = JSON.parse(fs.readFileSync("./commands.json"));
-console.log("Loaded " + commands.audio_play.length + " audio commands.");
-
-function saveCommands()
+//Load commands if exist
+if (fs.existsSync("./serverConfigs.json"))
 {
-  fs.mkdir("./Log", function(err) {
-    fs.rename("./commands.json", "./Log/commands" + new Date() + ".json", function(err) {
-      if (err) throw err;
-      fs.writeFile("./commands.json", JSON.stringify(commands), function(err) {
-        if (err) throw err;
-      });
-    });
-  });
+  var serverConfigs = JSON.parse(fs.readFileSync("./serverConfigs.json"));
+}
+else
+{
+  var serverConfigs = {
+    servers: []
+  };
+}
+console.log("Loaded " + serverConfigs.servers.length + " server configs.");
+
+client.on('message', onMessage);
+var commands = [
+  {
+    keyword: "save",
+    action: saveServerConfig
+  },
+  {
+    keyword: "add",
+    action: attemptAddContent
+  }
+];
+function onMessage(message)
+{
+    if (message.content.substring(0, 1) == "!")
+    {
+      var args = message.content.substring(1).split(" ");
+      for (let i = 0; i < commands.length; i++)
+      {
+        if (commands[i].keyword == args[0].toLowerCase())
+        {
+          //Command match
+          commands[i].action(message, args.slice(1));
+          break;
+        }
+      }
+    }
 }
 
-saveCommands();
+function attemptAddContent(message, args)
+{
+
+}
+
+function saveServerConfig(message, args)
+{
+  fs.writeFile("./serverConfigs.json", JSON.stringify(serverConfigs), function(err) {
+    if (err) throw err;
+    message.reply("Saved successfully.");
+  });
+}
